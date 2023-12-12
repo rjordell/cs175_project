5609533cb4bb2cc6eac840a79942b0aa647125af
Jordell <rjordell@gmail.com>
2023-12-12 09:28:02 -0800

    messed with a bunch of css for each individual item in collections

--- src/components/DisplayCollectionInfo/Track.js
+++ src/components/DisplayCollectionInfo/Track.js
@@ -5,32 +5,26 @@ function Track({ track }) {
   //console.log(track);
   return (
     <div className="container track">
-      <div className="imageAndTitle">
+      <div className="imageAndTitle item">
         <img
           src={track.cover.url}
           className="coverImg track"
           alt=""
         />
 
-        <div className="now-playing__side">
-          <div className="containerHeader">{track.title}</div>
-          <div className="containerSubheader">
+        <div className="now-playing__side item">
+          <div className="containerHeader itemTitle">{track.title}</div>
+          <div className="containerSubheader item">
             {track.description}
           </div>
         </div>
       </div>
       <div className="everythingElse">
-        <div className="now-playing__side">
-          <div className="containerHeader">{track.type}</div>
-          <div className="containerSubheader">
-            {track.type}
-          </div>
+        <div className="now-playing__side type">
+          <div className="containerSubheader item">{track.type}</div>
         </div>
-        <div className="now-playing__side">
-          <div className="containerHeader">
-            
-          </div>
-          <div className="containerSubheader">{track.note}</div>
+        <div className="now-playing__side note">
+          <div className="containerSubheader note">{track.note}</div>
         </div>
       </div>
     </div>
